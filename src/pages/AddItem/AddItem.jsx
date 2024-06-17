dafa16cf911e7d9662b12a2f98e49f48751d7d5b
kyj <dbwjd338@gmail.com>
2024-06-17 22:06:30 +0900

    충돌 수정

--- src/pages/AddItem/AddItem.jsx
+++ src/pages/AddItem/AddItem.jsx
@@ -1,21 +1,6 @@
-<<<<<<< HEAD
 import { useState } from 'react';
 import FileInput from './FileInput';
 import styles from '../../styles/AddItem.module.css';
-=======
-import { useState } from "react";
-import FileInput from "./FileInput";
-import InnerContainer from "../../styles/InnerContainer";
-import {
-  AddItemWrap,
-  AddItemTop,
-  Title,
-  Button,
-  Label,
-  Input,
-  Textarea,
-} from "../../styles/AddItemStyled";
->>>>>>> 80e16dd133dfcef5843560c738d0543da835110b
 
 function AddItem() {
   const [values, setValues] = useState({
@@ -30,7 +15,6 @@ function AddItem() {
   };
 
   return (
-<<<<<<< HEAD
     <main className="wrapper">
       <div className="inner">
         <div className={styles.addItemWrap}>
@@ -76,51 +60,6 @@ function AddItem() {
         </div>
       </div>
     </main>
-=======
-    <InnerContainer>
-      <AddItemWrap>
-        <form>
-          <AddItemTop>
-            <Title>상품 등록하기</Title>
-            <Button type="submit">등록</Button>
-          </AddItemTop>
-
-          <Label>상품 이미지</Label>
-          <FileInput
-            name="imgFile"
-            value={values.imgFile}
-            onChange={handleChange}
-          />
-          <Label>상품명</Label>
-          <Input
-            type="text"
-            name="item_name"
-            placeholder="상품명을 입력해주세요"
-          />
-
-          <Label>상품 소개</Label>
-          <Textarea
-            name="item_content"
-            placeholder="상품 소개를 입력해주세요"
-          ></Textarea>
-
-          <Label>판매가격</Label>
-          <Input
-            type="number"
-            name="item_price"
-            placeholder="판매 가격을 입력해주세요"
-          />
-
-          <Label>태그</Label>
-          <Input
-            type="text"
-            name="item_tag"
-            placeholder="태그를 입력해주세요"
-          />
-        </form>
-      </AddItemWrap>
-    </InnerContainer>
->>>>>>> 80e16dd133dfcef5843560c738d0543da835110b
   );
 }
 
